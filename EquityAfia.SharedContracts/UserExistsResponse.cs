fd78236c1aec2078efc98b617542ef7bb7b0bdae
Abisagy Nafula <abisakyn@gmail.com>
2024-07-12 15:24:47 +0300

    Testing

---
+++ EquityAfia.SharedContracts/UserExistsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace EquityAfia.SharedContracts
+{
+    public class UserExistsResponse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string IdNumber { get; set; }
+        public bool Exists { get; set; }
+    }
+}
